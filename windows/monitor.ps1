2c0db6124e89e77bbd04185368c0da3b5fc50cb3
Jainil Jakasaniya (jjakasan) <jainiljakasaniya@gmail.com>
2024-02-20 14:45:10 +0530

    update script

--- windows/monitor.ps1
+++ windows/monitor.ps1
@@ -1,7 +1,7 @@
-Get-CimInstance -Namespace root\wmi -ClassName wmimonitorid | foreach {
-    $Manufacturer = if ($_.ManufacturerName -notmatch '^0$') { ($_.ManufacturerName | foreach {[char]$_}) -join "" } else { "" }
-    $Name = if ($_.UserFriendlyName -notmatch '^0$') { ($_.UserFriendlyName | foreach {[char]$_}) -join "" } else { "" }
-    $Serial = if ($_.SerialNumberID -notmatch '^0$') { ($_.SerialNumberID | foreach {[char]$_}) -join "" } else { "" }
+Get-CimInstance -Namespace root\wmi -ClassName wmimonitorid | ForEach-Object {
+    $Manufacturer = if ($_.ManufacturerName -notmatch '^0$') { ($_.ManufacturerName | ForEach-Object {[char]$_}) -join "" } else { "" }
+    $Name = if ($_.UserFriendlyName -notmatch '^0$') { ($_.UserFriendlyName | ForEach-Object {[char]$_}) -join "" } else { "" }
+    $Serial = if ($_.SerialNumberID -notmatch '^0$') { ($_.SerialNumberID | ForEach-Object {[char]$_}) -join "" } else { "" }
 
     New-Object -TypeName psobject -Property @{
         Manufacturer = $Manufacturer
